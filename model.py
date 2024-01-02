735e71235ed6d8873dac2a2f679f537a032e61d7
zeeraknaveed <znaveed.bee17seecs@seecs.edu.pk>
2024-01-02 21:43:54 +0900

    hyperparameters changed

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from train_classical import n_qubits
 
 class MyClassicalFC(nn.Module):
     def __init__(self):
         super(MyClassicalFC, self).__init__()
         
         # First linear layer with 512 input and 64 output
-        self.fc1 = nn.Linear(512, 64)
+        self.fc1 = nn.Linear(512, n_qubits)
         
         # Last linear layer with 64 input and 2 output
-        self.fc2 = nn.Linear(64, 2)
+        self.fc2 = nn.Linear(n_qubits, 2)
     
     def forward(self, x):
         # Forward pass with softmax activation
